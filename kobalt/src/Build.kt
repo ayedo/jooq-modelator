6fbe2c914daf1a00b07fb45b13c7645f903dd63e
Alon Dolev <alon@dolev.ch>
2017-06-11 20:42:59 +0200

    adds dependencies

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -10,6 +10,10 @@ val modelator = project {
 
     dependencies {
         compile("org.jetbrains.kotlin:kotlin-stdlib:1.1.2-4")
+        compile("net.jodah:failsafe:0.9.1")
+        compile("com.spotify:docker-client:8.1.2")
+        compile("org.flywaydb:flyway-core:4.1.2")
+        compile("org.jooq:jooq-codegen:3.9.1")
     }
 
     dependenciesTest {
