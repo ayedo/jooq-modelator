53c8c7dccdd2da5e1a22a04eef619b17815e593e
Alon Dolev <alon@dolev.ch>
2018-09-24 09:13:02 +0200

    adds a warning when more than one container are found with same tag value

--- src/main/kotlin/ch/ayedo/jooqmodelator/core/Modelator.kt
+++ src/main/kotlin/ch/ayedo/jooqmodelator/core/Modelator.kt
@@ -3,8 +3,11 @@ package ch.ayedo.jooqmodelator.core
 import ch.ayedo.jooqmodelator.core.configuration.Configuration
 import ch.ayedo.jooqmodelator.core.configuration.DatabaseConfig
 import com.spotify.docker.client.DefaultDockerClient
+import org.slf4j.LoggerFactory
 
 class Modelator(configuration: Configuration) {
+
+    private val log = LoggerFactory.getLogger(Modelator::class.java)
 
     private val dockerConfig = configuration.dockerConfig
 
@@ -32,7 +35,10 @@ class Modelator(configuration: Configuration) {
                 if (existingContainers.isEmpty()) {
                     docker.createContainer(dockerConfig.toContainerConfig()).id()!!
                 } else {
-                    // TODO: warn if more than one found
+                    if (existingContainers.size > 1) {
+                        log.warn("More than one container with tag ${dockerConfig.labelKey}=$tag has been found. " +
+                            "Using the one which was most recently created")
+                    }
                     existingContainers.sortedBy({ it.created() }).map({ it.id() }).first()
                 }
 
