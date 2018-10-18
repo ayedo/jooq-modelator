f1dc6c40b6ae4782b4f9a038ba1b899508f78918
Alon Dolev <alon@dolev.ch>
2018-10-18 09:19:10 +0200

    adds jOOQ configuration parsing that does not depend on JAXBContext

--- src/main/kotlin/ch/ayedo/jooqmodelator/core/configuration/DatabaseConfiguration.kt
+++ src/main/kotlin/ch/ayedo/jooqmodelator/core/configuration/DatabaseConfiguration.kt
@@ -2,48 +2,53 @@ package ch.ayedo.jooqmodelator.core.configuration
 
 import java.nio.charset.Charset
 import java.nio.file.Path
-import javax.xml.bind.JAXBContext
 import javax.xml.stream.XMLInputFactory
+import javax.xml.stream.XMLStreamReader
 
-/** The database configuration is loaded from the jooq configuration file */
-@javax.xml.bind.annotation.XmlAccessorType(javax.xml.bind.annotation.XmlAccessType.FIELD)
-class DatabaseConfig {
-
-    var driver: String? = null
-    var url: String? = null
-    var user: String? = null
-    var password: String? = null
+data class DatabaseConfig(val driver: String, val url: String, val user: String, val password: String) {
 
     companion object {
 
         fun fromJooqConfig(jooqConfigPath: Path): DatabaseConfig {
 
-            val xmlStreamReader = XMLInputFactory.newFactory().let { factory ->
-                val reader = jooqConfigPath.toFile().reader(Charset.forName("UTF-8"))
-                factory.createXMLStreamReader(reader)
-            }
+            val factory = XMLInputFactory.newFactory()
 
-            try {
-                xmlStreamReader.nextTag()
+            val reader = jooqConfigPath.toFile().reader(Charset.forName("UTF-8"))
 
-                while (xmlStreamReader.localName != "jdbc") {
-                    xmlStreamReader.nextTag()
-                }
+            val xmlStreamReader = factory.createXMLStreamReader(reader)
 
-                val unmarshaller = JAXBContext.newInstance(DatabaseConfig::class.java).createUnmarshaller()
-                val jb = unmarshaller.unmarshal(xmlStreamReader, DatabaseConfig::class.java)
+            return try {
 
-                val config = jb.value
+                skipToTag(xmlStreamReader, "jdbc")
 
-                if (config.url == null && config.user == null && config.password == null) {
-                    throw IllegalStateException("Could not parse database configuration from jooq configuration file." +
-                        "[unmarshaller=${unmarshaller::class.java} jooqConfigPath=${jooqConfigPath.toAbsolutePath()} content=${jooqConfigPath.toFile().readText()}]")
-                }
+                skipToTag(xmlStreamReader, "driver")
+                val driver = xmlStreamReader.elementText
 
-                return config
+                skipToTag(xmlStreamReader, "url")
+                val url = xmlStreamReader.elementText
 
+                skipToTag(xmlStreamReader, "user")
+                val user = xmlStreamReader.elementText
+
+                skipToTag(xmlStreamReader, "password")
+                val password = xmlStreamReader.elementText
+
+                DatabaseConfig(driver, url, user, password)
+
+            } catch (e: Exception) {
+                throw IllegalStateException("Could not parse database configuration from jooq configuration file.\n" +
+                    "[XMLStreamReader=${xmlStreamReader::class.java} jooqConfigPath=${jooqConfigPath.toAbsolutePath()}\n" +
+                    "content=${jooqConfigPath.toFile().readText()}]", e)
             } finally {
                 xmlStreamReader.close()
+            }
+        }
+
+        private fun skipToTag(xmlStreamReader: XMLStreamReader, tag: String) {
+            xmlStreamReader.nextTag()
+
+            while (xmlStreamReader.localName != tag) {
+                xmlStreamReader.nextTag()
             }
         }
     }
